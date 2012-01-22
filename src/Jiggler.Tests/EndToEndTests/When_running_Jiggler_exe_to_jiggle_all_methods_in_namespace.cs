10e2c407914018257ea687f7beb77b542fafd1e4
Adrian <Adrian>
2012-01-22 10:33:43 +0000

    Added TestAssembly and skelaton e2e test.

---
+++ src/Jiggler.Tests/EndToEndTests/When_running_Jiggler_exe_to_jiggle_all_methods_in_namespace.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+// ReSharper disable InconsistentNaming
+
+namespace Jiggler.Tests.EndToEndTests
+{
+    [TestFixture]
+    public abstract class JigglerExeEndToEndTests
+    {
+        [SetUp]
+        public void Context()
+        {
+            
+        }
+
+        [TestFixture]
+        public class When_jiggling_all_methods_in_a_namespace : JigglerExeEndToEndTests
+        {
+            
+        }
+    }
+}
---
+++ src/TestAssembly/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestAssembly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
