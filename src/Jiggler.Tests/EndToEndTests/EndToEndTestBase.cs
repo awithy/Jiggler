14ded1a9fc6c1b361460b94fd7f2ec6cc966466b
Adrian <Adrian>
2012-01-23 23:31:20 +0000

    Renamed e2e test.

---
+++ src/Jiggler.Tests/EndToEndTests/EndToEndTestBase.cs
@@ -0,0 +1,26 @@
+﻿using Jiggler.Tests.EndToEndTests.Helpers;
+using NUnit.Framework;
+// ReSharper disable InconsistentNaming
+// ReSharper disable ConvertToConstant.Local
+// ReSharper disable FieldCanBeMadeReadOnly.Local
+
+namespace Jiggler.Tests.EndToEndTests
+{
+    [TestFixture]
+    public abstract class EndToEndTestBase
+    {
+        protected JiggleExeRunner _jiggleExeRunner;
+        protected TestPathProvider _testPathProvider;
+        protected TestAssemblyExeRunner _testAssemblyExeRunner;
+        protected string _testAssemblyNamespace = "NamespaceToJiggle";
+        protected string _jiggleMethod = "TestAssembly.TestJiggle.Jiggle";
+
+        [SetUp]
+        public void SharedContext()
+        {
+            _jiggleExeRunner = new JiggleExeRunner();
+            _testPathProvider = new TestPathProvider();
+            _testAssemblyExeRunner = new TestAssemblyExeRunner();
+        }
+    }
+}
---
+++ src/Jiggler.Tests/EndToEndTests/When_jiggling_all_methods_in_namespace.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using Jiggler.Tests.EndToEndTests.Helpers;
+using NUnit.Framework;
+// ReSharper disable InconsistentNaming
+// ReSharper disable ConvertToConstant.Local
+// ReSharper disable FieldCanBeMadeReadOnly.Local
+
+namespace Jiggler.Tests.EndToEndTests
+{
+    [TestFixture]
+    public class When_jiggling_all_methods_in_a_namespace : EndToEndTestBase
+    {
+        private string _randomStringToPassThroughForTest = Guid.NewGuid().ToString("N");
+        private string _outputTestFileContents;
+
+        [SetUp]
+        public void Context()
+        {
+            _JiggleTestAssembly();
+            _ProduceTestJiggleOutputUsingJiggledTestAssembly();
+        }
+
+        private void _JiggleTestAssembly()
+        {
+            var jiggleArguments = new JiggleExeRunnerArguments
+                                      {
+                                          AssemblyPath = _testPathProvider.GetTestAssemblyBuildPath(),
+                                          Namespace = _testAssemblyNamespace,
+                                          JiggleMethod = _jiggleMethod,
+                                      };
+            _jiggleExeRunner.Run(jiggleArguments);
+        }
+
+        private void _ProduceTestJiggleOutputUsingJiggledTestAssembly()
+        {
+            _RunTestAssembly();
+            _ReadJiggleTestOutput();
+        }
+
+        private void _RunTestAssembly()
+        {
+            _testAssemblyExeRunner.Run(_randomStringToPassThroughForTest);
+        }
+
+        private void _ReadJiggleTestOutput()
+        {
+            var pathToTestOutputFile = _testPathProvider.GetTestAssemblyJiggleTestFile();
+            _outputTestFileContents = File.ReadAllText(pathToTestOutputFile);
+        }
+
+        [Test]
+        public void It_should_add_a_call_to_the_jiggle_method_to_every_method_in_the_system_()
+        {
+            Assert.That(_outputTestFileContents, Is.EqualTo(_randomStringToPassThroughForTest));
+        }
+    }
+}
